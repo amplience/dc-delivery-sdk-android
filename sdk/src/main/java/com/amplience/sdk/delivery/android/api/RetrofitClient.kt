63b2773a06b90ed0daab5bcc44424f171b2b97eb
Dylan McKee <dylan@djmckee.co.uk>
2022-09-29 11:14:34 +0100

    refactor: lift out the client construction into a function

--- sdk/src/main/java/com/amplience/sdk/delivery/android/api/RetrofitClient.kt
+++ sdk/src/main/java/com/amplience/sdk/delivery/android/api/RetrofitClient.kt
@@ -10,55 +10,50 @@ import java.util.concurrent.TimeUnit
 internal object RetrofitClient {
 
     // Store retrofit instances by their base url, separated by cache or fresh
-    private var cacheClients: HashMap<String, Retrofit?> = hashMapOf()
-    private var freshClients: HashMap<String, Retrofit?> = hashMapOf()
+    private var cacheClients: HashMap<String, Retrofit> = hashMapOf()
+    private var freshClients: HashMap<String, Retrofit> = hashMapOf()
 
     fun getClient(baseUrl: String, freshApiKey: String? = null): Retrofit {
-        // Find existing client in relevant map depending on whether fresh api key is present
-        val existing = if (freshApiKey == null) {
-            cacheClients[baseUrl]
+        val cache = if (freshApiKey == null) cacheClients else freshClients
+
+        val existingClient = cache[baseUrl]
+
+        return if (existingClient != null) {
+            existingClient
         } else {
-            freshClients[baseUrl]
+            val client = newClient(baseUrl, freshApiKey)
+            cache[baseUrl] = client
+            client
         }
-        if (existing != null) {
-            return existing
-        } else {
-            val interceptor = HttpLoggingInterceptor()
-            interceptor.setLevel(HttpLoggingInterceptor.Level.HEADERS)
-            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
 
-            val client = OkHttpClient.Builder().connectTimeout(30, TimeUnit.SECONDS)
-                .writeTimeout(30, TimeUnit.SECONDS)
-                .readTimeout(30, TimeUnit.SECONDS)
+    private fun newClient(baseUrl: String, freshApiKey: String? = null): Retrofit {
+        val client = OkHttpClient.Builder().connectTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
 
-            client.addInterceptor(interceptor)
-            if (freshApiKey != null) {
-                client.addInterceptor { chain ->
-                    val requestBuilder = chain.request().newBuilder()
-                    requestBuilder.header("X-API-Key", freshApiKey)
-                    chain.proceed(requestBuilder.build())
-                }
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.HEADERS)
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        client.addInterceptor(interceptor)
+
+        if (freshApiKey != null) {
+            client.addInterceptor { chain ->
+                val requestBuilder = chain.request().newBuilder()
+                requestBuilder.header("X-API-Key", freshApiKey)
+                chain.proceed(requestBuilder.build())
             }
-
-            val gson = GsonBuilder()
-                .setLenient()
-                .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")
-                .create()
-
-            val new = Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .client(client.build())
-                .build()
-
-            // Store new client in relevant map
-            if (freshApiKey == null) {
-                cacheClients[baseUrl] = new
-            } else {
-                freshClients[baseUrl] = new
-            }
-
-            return new
         }
+
+        val gson = GsonBuilder()
+            .setLenient()
+            .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")
+            .create()
+
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .client(client.build())
+            .build()
     }
 }
